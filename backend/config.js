81c25e9567442e3b14dfc85606bed085d3800c44
ricardoj12-s <ricardojosueh12@hotmail.com>
2024-07-25 22:14:19 -0400

    Fix Procfile path

--- backend/config.js
+++ backend/config.js
@@ -5,6 +5,8 @@ module.exports = {
     jwtExpirySeconds: parseInt(process.env.JWT_EXPIRY_SECONDS, 10) || 3600,
     apiKeys: {
         openWeatherMap: process.env.OPENWEATHERMAP_API_KEY || '9f0699e75c0ca345057344fac67123d7',
+        GOOGLE_MAPS_API_KEY: process.env.GOOGLE_MAPS_API_KEY || 'AIzaSyCaFgo3l3atkS8bsuZGOOKDAI6hGrmJCgc'
+
     },
     db: {
         host: process.env.DB_HOST || 'localhost',
